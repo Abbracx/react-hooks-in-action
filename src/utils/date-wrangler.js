cb1056f6713bec1474f5104dd9dd5128552eae1f
Abbracx <tankoraphael@gmail.com>
2023-01-10 07:11:17 +0100

    Created utility functions to work with dates and weeks

---
+++ src/utils/date-wrangler.js
@@ -0,0 +1,26 @@
+
+export function addDays (date, daysToAdd) {
+    const clone = new Date(date.getTime());
+    clone.setDate(clone.getDate() + daysToAdd); // Shift the date by the number of days specified.
+    return clone;
+}
+
+export function getWeek (forDate, daysOffset = 0){
+    const date = addDays(forDate, daysOffset)
+    const day = date.getDay(); // Get the day index for the new date, for example, Tuesday = 2.
+
+    return {
+        date,
+        start: addDays(date, -day), // For example, if it’s Tuesday, shift back by 2 days.
+        end: addDays(date, 6 - day) // For example, if it’s Tuesday, shift forward by 4 days.
+    };
+}
+
+// Get the week object for the week containing today’s date
+// const today = new Date();
+// const week = getWeek(today)
+
+
+// Get the week object for the week containing the date a week from today
+// const today = new Date();
+// const week = getWeek(today, 7);
