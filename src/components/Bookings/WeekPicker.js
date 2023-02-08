3d27b6453b94a338bb3b5dba67d0e37266e88ad1
Abbracx <tankoraphael@gmail.com>
2023-02-08 14:14:18 +0100

    update WeekPicker to be able to pick specific date

--- src/components/Bookings/WeekPicker.js
+++ src/components/Bookings/WeekPicker.js
@@ -1,11 +1,34 @@
-import { useReducer } from "react";
+import { useReducer, useRef, useState } from "react";
 import reducer from "./weekReducer";
 import { getWeek } from "../../utils/date-wrangler";
-import {FaChevronLeft, FaCalendarDay, FaChevronRight} from "react-icons/fa";
+import {
+    FaChevronLeft, 
+    FaCalendarDay, 
+    FaChevronRight, 
+    FaCalendarCheck 
+} from "react-icons/fa";
 
 
 export default function WeekPicker({date}) {
-    const [week, dispatch] = useReducer(reducer, date, getWeek) // Generate the initial state, passing date to getWeek.
+
+    // Generate the initial state, passing date to getWeek.
+    const [week, dispatch] = useReducer(reducer, date, getWeek) 
+    const [dateText, setDateText] = useState(new Date())
+    // const textboxRef = useRef();
+
+    /* 
+        1. Define a handler for the Go button
+        2. Dispatch the SET_DATE action.
+        3. Use the ref to get the text value in the text box.
+    */
+
+    function gotoDate() {
+        dispatch({
+            type: "SET_DATE",
+            payload: dateText
+            // payload: textboxRef.current.value
+        })
+    }
 
     return (
         <div>
@@ -25,6 +48,25 @@ export default function WeekPicker({date}) {
                     <FaCalendarDay/>
                     <span>Today</span>
                 </button>
+
+                <span>
+                    <input 
+                        type="text"
+                        // ref={textboxRef}
+                        placeholder="e.g. 2020-09-02"
+                        // defaultValue={new Date()}
+                        value={dateText}
+                        onChange={(e) => setDateText(e.target.value)}
+                    />
+
+                    <button
+                        className="go btn"
+                        onClick={gotoDate}
+                    >
+                        <FaCalendarCheck/>
+                        <span>Go</span>
+                    </button>
+                </span>
 
                 <button 
                     className="btn"
