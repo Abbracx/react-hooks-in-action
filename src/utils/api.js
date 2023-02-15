7c92d3be865e07025aa76d941a7cfe01ce1c0be9
Abbracx <tankoraphael@gmail.com>
2023-02-15 16:19:34 +0100

    Added a getBookings data-loading function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,5 @@
+import {shortISO} from "./date-wrangler";
+
 export default function getData(url){
     return fetch(url)
         .then(resp => {
@@ -6,4 +8,19 @@ export default function getData(url){
             }
             return resp.json();
         })
+}
+
+export  function getBookings(bookableId, startDate, endDate){
+
+    // Format the dates for the querystring
+    const start = shortISO(startDate); 
+    const end = shortISO(endDate);
+
+    const urlRoot = "http://localhost:3001/bookings";
+
+    // Build up the query string.
+    const query = `bookableId=${bookableId}&date_gte=${start}&date_lte=${end}`;
+
+    // Fetch the bookings, returning a promise.
+    return getData(`${urlRoot}?${query}`);
 }
